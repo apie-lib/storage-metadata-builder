b2277d4b0be5016b544754323846a92e7c012614
pjordaan <pjordaan@users.noreply.github.com>
2024-11-20 05:06:58 +0000

    Update file(s) "packages/storage-metadata-builder/." from "apie-lib/apie-lib-monorepo"

--- src/CodeGenerators/FileTableGenerator.php
+++ src/CodeGenerators/FileTableGenerator.php
@@ -4,6 +4,7 @@ namespace Apie\StorageMetadataBuilder\CodeGenerators;
 use Apie\Core\FileStorage\FileStorageInterface;
 use Apie\Core\FileStorage\StoredFile;
 use Apie\Core\Identifiers\KebabCaseSlug;
+use Apie\Core\Lists\IntegerHashmap;
 use Apie\Core\Utils\ConverterUtils;
 use Apie\StorageMetadata\Attributes\GetMethodOrPropertyAttribute;
 use Apie\StorageMetadata\Attributes\OneToOneAttribute;
@@ -11,11 +12,53 @@ use Apie\StorageMetadata\Attributes\StorageMappingAttribute;
 use Apie\StorageMetadataBuilder\Factories\ClassTypeFactory;
 use Apie\StorageMetadataBuilder\Interfaces\RunGeneratedCodeContextInterface;
 use Apie\StorageMetadataBuilder\Mediators\GeneratedCodeContext;
+use Nette\PhpGenerator\ClassType;
 use Psr\Http\Message\UploadedFileInterface;
 use ReflectionClass;
+use ReflectionProperty;
 
 final class FileTableGenerator implements RunGeneratedCodeContextInterface
 {
+    private const ALT_TYPES = [
+        'fileSize' => '?int',
+        'indexing' => '?' . IntegerHashmap::class,
+        'storagePath' => 'string'
+    ];
+
+    private function createFileProperties(ReflectionClass $class): array
+    {
+        return [
+            'storagePath' => ['getStoragePath', 'storagePath', $class->name, 'allowLargeStrings' => true],
+            'clientMimeType' => ['getClientMediaType', 'clientMimeType', $class->name],
+            'clientOriginalFile' => ['getClientFilename', 'clientOriginalFile', $class->name],
+            'fileSize' => ['getSize', 'fileSize', $class->name],
+            'serverMimeType' => ['getServerMimeType', 'serverMimeType', $class->name],
+            'serverPath' => ['getServerPath', 'serverPath', $class->name],
+            'indexing' => ['getIndexing', 'indexing', $class->name],
+        ];
+    }
+
+    private function applyProperty(ClassType $table, ReflectionProperty $property, ReflectionClass $class): void
+    {
+        if (!$table->hasProperty('storage')) {
+            $table->addProperty('storage')
+                ->setType('?' . FileStorageInterface::class)
+                ->addAttribute(StorageMappingAttribute::class);
+        }
+
+        $properties = $this->createFileProperties($class);
+        $propertyName = $property->name;
+        $type = self::ALT_TYPES[$propertyName] ?? '?string';
+        if (isset($properties[$propertyName])) {
+            $table->addProperty($propertyName)
+                ->setType($type)
+                ->addAttribute(
+                    GetMethodOrPropertyAttribute::class,
+                    $properties[$propertyName]
+                );
+        }
+    }
+
     public function run(GeneratedCodeContext $generatedCodeContext): void
     {
         $property = $generatedCodeContext->getCurrentProperty();
@@ -25,33 +68,24 @@ final class FileTableGenerator implements RunGeneratedCodeContextInterface
         }
         $class = ConverterUtils::toReflectionClass($property);
         if (!$class) {
+            if ($property->getDeclaringClass()->name === StoredFile::class) {
+                $this->applyProperty($currentTable, $property, new ReflectionClass(StoredFile::class));
+            }
             return;
         }
         if ($class->name === UploadedFileInterface::class) {
             $class = new ReflectionClass(StoredFile::class);
         }
         if (in_array(UploadedFileInterface::class, $class->getInterfaceNames())) {
+            // TODO: is this still needed?
             $tableName = $generatedCodeContext->getPrefix('apie_resource_');
             $table = ClassTypeFactory::createStorageTable($tableName, new ReflectionClass(StoredFile::class));
             $table->addProperty('storage')
                 ->setType('?' . FileStorageInterface::class)
                 ->addAttribute(StorageMappingAttribute::class);
-            $properties = [
-                'storagePath' => ['getStoragePath', 'storagePath', $class->name, 'allowLargeStrings' => true],
-                'clientMimeType' => ['getClientMediaType', 'clientMimeType', $class->name],
-                'clientOriginalFile' => ['getClientFilename', 'clientOriginalFile', $class->name],
-                'fileSize' => ['getSize', 'fileSize', $class->name],
-                'serverMimeType' => ['getServerMimeType', 'serverMimeType', $class->name],
-                'serverPath' => ['getServerPath', 'serverPath', $class->name],
-                'indexing' => ['getIndexing', 'indexing', $class->name],
-            ];
-            $types = [
-                'fileSize' => '?int',
-                'indexing' => 'array',
-                'storagePath' => 'string'
-            ];
+            $properties = $this->createFileProperties($class);
             foreach ($properties as $propertyName => $attributeArguments) {
-                $type = $types[$propertyName] ?? '?string';
+                $type = self::ALT_TYPES[$propertyName] ?? '?string';
                 $table->addProperty($propertyName)
                     ->setType($type)
                     ->addAttribute(
