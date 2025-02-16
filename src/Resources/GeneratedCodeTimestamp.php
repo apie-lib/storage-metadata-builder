20107086955ec2535214825ce43a95a3fc2bb899
pjordaan <pjordaan@users.noreply.github.com>
2025-02-16 15:59:57 +0000

    Update file(s) "packages/storage-metadata-builder/." from "apie-lib/apie-lib-monorepo"

--- src/Resources/GeneratedCodeTimestamp.php
+++ src/Resources/GeneratedCodeTimestamp.php
@@ -1,19 +1,26 @@
 <?php
 namespace Apie\StorageMetadataBuilder\Resources;
 
+use Apie\Core\Attributes\ProvideIndex;
 use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Lists\StringHashmap;
 use Apie\StorageMetadataBuilder\Identifiers\GeneratedCodeTimestampId;
 use Apie\StorageMetadataBuilder\Lists\GeneratedCodeHashmap;
 
+#[ProvideIndex('noIndexing')]
 class GeneratedCodeTimestamp implements EntityInterface
 {
     private GeneratedCodeTimestampId $id;
-    public StringHashmap $codeMap;
+    private StringHashmap $codeMap;
     public function __construct(GeneratedCodeHashmap $generatedCodeHashmap)
     {
         $this->codeMap = $generatedCodeHashmap->toStringHashmap();
         $this->id = GeneratedCodeTimestampId::createFromMap($this->codeMap);
+    }
+
+    public static function noIndexing(): array
+    {
+        return [];
     }
 
     public function getId(): GeneratedCodeTimestampId
